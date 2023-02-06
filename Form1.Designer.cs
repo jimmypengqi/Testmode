d47d6bb58f37966dc931ce2b7a66b926eb12dee6
peng jimmy <jimmy.peng2020@outlook.com>
2023-02-06 18:31:48 +0800

    提交添加项目文件。

---
+++ Form1.Designer.cs
@@ -0,0 +1,181 @@
+﻿
+namespace 播放器
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.VideoPlayer = new AxWMPLib.AxWindowsMediaPlayer();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.删除ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.uiButton1 = new Sunny.UI.UIButton();
+            this.uiButton2 = new Sunny.UI.UIButton();
+            this.uiButton3 = new Sunny.UI.UIButton();
+            this.uiRoundProcess1 = new Sunny.UI.UIRoundProcess();
+            this.uiRoundProcess2 = new Sunny.UI.UIRoundProcess();
+            this.uiRoundProcess3 = new Sunny.UI.UIRoundProcess();
+            ((System.ComponentModel.ISupportInitialize)(this.VideoPlayer)).BeginInit();
+            this.contextMenuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // VideoPlayer
+            // 
+            this.VideoPlayer.Enabled = true;
+            this.VideoPlayer.Location = new System.Drawing.Point(3, 38);
+            this.VideoPlayer.Name = "VideoPlayer";
+            this.VideoPlayer.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("VideoPlayer.OcxState")));
+            this.VideoPlayer.Size = new System.Drawing.Size(531, 457);
+            this.VideoPlayer.TabIndex = 0;
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.DropShadowEnabled = false;
+            this.contextMenuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.删除ToolStripMenuItem});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(109, 28);
+            // 
+            // 删除ToolStripMenuItem
+            // 
+            this.删除ToolStripMenuItem.Name = "删除ToolStripMenuItem";
+            this.删除ToolStripMenuItem.Size = new System.Drawing.Size(108, 24);
+            this.删除ToolStripMenuItem.Text = "删除";
+            this.删除ToolStripMenuItem.Click += new System.EventHandler(this.删除ToolStripMenuItem_Click);
+            // 
+            // uiButton1
+            // 
+            this.uiButton1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.uiButton1.Font = new System.Drawing.Font("微软雅黑", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.uiButton1.Location = new System.Drawing.Point(540, 290);
+            this.uiButton1.MinimumSize = new System.Drawing.Size(1, 1);
+            this.uiButton1.Name = "uiButton1";
+            this.uiButton1.Radius = 30;
+            this.uiButton1.Size = new System.Drawing.Size(121, 40);
+            this.uiButton1.TabIndex = 1;
+            this.uiButton1.Text = "垃圾投放口";
+            this.uiButton1.TipsFont = new System.Drawing.Font("微软雅黑", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.uiButton1.ZoomScaleRect = new System.Drawing.Rectangle(0, 0, 0, 0);
+            // 
+            // uiButton2
+            // 
+            this.uiButton2.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.uiButton2.Font = new System.Drawing.Font("微软雅黑", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.uiButton2.Location = new System.Drawing.Point(671, 290);
+            this.uiButton2.MinimumSize = new System.Drawing.Size(1, 1);
+            this.uiButton2.Name = "uiButton2";
+            this.uiButton2.Radius = 30;
+            this.uiButton2.Size = new System.Drawing.Size(121, 40);
+            this.uiButton2.TabIndex = 2;
+            this.uiButton2.Text = "厨余投放口";
+            this.uiButton2.TipsFont = new System.Drawing.Font("微软雅黑", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.uiButton2.ZoomScaleRect = new System.Drawing.Rectangle(0, 0, 0, 0);
+            // 
+            // uiButton3
+            // 
+            this.uiButton3.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.uiButton3.Font = new System.Drawing.Font("微软雅黑", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.uiButton3.Location = new System.Drawing.Point(808, 290);
+            this.uiButton3.MinimumSize = new System.Drawing.Size(1, 1);
+            this.uiButton3.Name = "uiButton3";
+            this.uiButton3.Radius = 30;
+            this.uiButton3.Size = new System.Drawing.Size(121, 40);
+            this.uiButton3.TabIndex = 3;
+            this.uiButton3.Text = "被服投放口";
+            this.uiButton3.TipsFont = new System.Drawing.Font("微软雅黑", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.uiButton3.ZoomScaleRect = new System.Drawing.Rectangle(0, 0, 0, 0);
+            // 
+            // uiRoundProcess1
+            // 
+            this.uiRoundProcess1.Font = new System.Drawing.Font("微软雅黑", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.uiRoundProcess1.Location = new System.Drawing.Point(540, 135);
+            this.uiRoundProcess1.MinimumSize = new System.Drawing.Size(1, 1);
+            this.uiRoundProcess1.Name = "uiRoundProcess1";
+            this.uiRoundProcess1.Size = new System.Drawing.Size(123, 116);
+            this.uiRoundProcess1.TabIndex = 4;
+            this.uiRoundProcess1.Text = "uiRoundProcess1";
+            this.uiRoundProcess1.ZoomScaleRect = new System.Drawing.Rectangle(0, 0, 0, 0);
+            // 
+            // uiRoundProcess2
+            // 
+            this.uiRoundProcess2.Font = new System.Drawing.Font("微软雅黑", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.uiRoundProcess2.Location = new System.Drawing.Point(808, 135);
+            this.uiRoundProcess2.MinimumSize = new System.Drawing.Size(1, 1);
+            this.uiRoundProcess2.Name = "uiRoundProcess2";
+            this.uiRoundProcess2.Size = new System.Drawing.Size(123, 116);
+            this.uiRoundProcess2.TabIndex = 5;
+            this.uiRoundProcess2.Text = "uiRoundProcess2";
+            this.uiRoundProcess2.ZoomScaleRect = new System.Drawing.Rectangle(0, 0, 0, 0);
+            // 
+            // uiRoundProcess3
+            // 
+            this.uiRoundProcess3.Font = new System.Drawing.Font("微软雅黑", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.uiRoundProcess3.Location = new System.Drawing.Point(669, 135);
+            this.uiRoundProcess3.MinimumSize = new System.Drawing.Size(1, 1);
+            this.uiRoundProcess3.Name = "uiRoundProcess3";
+            this.uiRoundProcess3.Size = new System.Drawing.Size(123, 116);
+            this.uiRoundProcess3.TabIndex = 6;
+            this.uiRoundProcess3.Text = "uiRoundProcess3";
+            this.uiRoundProcess3.ZoomScaleRect = new System.Drawing.Rectangle(0, 0, 0, 0);
+            // 
+            // Form1
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.ClientSize = new System.Drawing.Size(951, 498);
+            this.Controls.Add(this.uiRoundProcess3);
+            this.Controls.Add(this.uiRoundProcess2);
+            this.Controls.Add(this.uiRoundProcess1);
+            this.Controls.Add(this.uiButton3);
+            this.Controls.Add(this.uiButton2);
+            this.Controls.Add(this.uiButton1);
+            this.Controls.Add(this.VideoPlayer);
+            this.Name = "Form1";
+            this.Text = "投放终端";
+            this.ZoomScaleRect = new System.Drawing.Rectangle(19, 19, 843, 450);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.VideoPlayer)).EndInit();
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private AxWMPLib.AxWindowsMediaPlayer VideoPlayer;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem 删除ToolStripMenuItem;
+        private Sunny.UI.UIButton uiButton1;
+        private Sunny.UI.UIButton uiButton2;
+        private Sunny.UI.UIButton uiButton3;
+        private Sunny.UI.UIRoundProcess uiRoundProcess1;
+        private Sunny.UI.UIRoundProcess uiRoundProcess2;
+        private Sunny.UI.UIRoundProcess uiRoundProcess3;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,37 @@
+﻿using Sunny.UI;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace 播放器
+{
+    public partial class Form1 : UIForm
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            VideoPlayer.settings.setMode("loop",true);
+         
+        }
+
+        private void 删除ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            VideoPlayer.TabStop = false;
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
